fe4b20895a8141c4be1f3b2ebfb1f0334a6453ff
KabakiAntony <kabaki.antony@gmail.com>
2022-06-17 19:32:37 +0200

    created a utils file containing openAction for the dashboard

---
+++ src/utils/index.js
@@ -0,0 +1,14 @@
+export function openAction(evt, actionName){
+    let i, actioncontent, actionlinks;
+    actioncontent = document.getElementsByClassName("actionContent");
+    for (i = 0; i < actioncontent.length; i++) {
+        actioncontent[i].style.display = "none";
+    }
+    actionlinks = document.getElementsByClassName("action-links");
+    for (i = 0; i < actionlinks.length; i++) {
+        actionlinks[i].className = actionlinks[i].className.replace(" active", "");
+    }
+    document.getElementById(actionName).style.display = "block";
+    document.getElementById('profileInstruction').style.display = "none";
+    evt.currentTarget.className += " active";
+  };
